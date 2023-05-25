87cde820bd19c48ce0078d6246acf0d0c583a6bb
sahibjotmaan <sin13877@shernet.sheridancollege.ca>
2023-05-25 15:45:36 -0400

    student details version 1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author sahibjotsingh
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("software course");
+        System.out.println("Hahhaha");
+    }
+    
+}
